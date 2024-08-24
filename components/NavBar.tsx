2bf357ef7517f08001d426bd8dc52c2aa858a3c3
suneelfm <sfm.20031998@gmail.com>
2024-08-24 12:34:53 +0530

    Created NavBar and layout

---
+++ components/NavBar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import {
+  DescriptionRounded,
+  FactoryRounded,
+  HouseRounded,
+  LayersRounded,
+  ShowChartRounded,
+  TodayRounded,
+} from "@mui/icons-material";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+import styles from "../styles/layout.module.css";
+
+export default function NavBar() {
+  const pathName = usePathname();
+  interface Pages {
+    icon: ReactNode;
+    path: string;
+  }
+
+  const PAGES: Pages[] = [
+    { icon: <HouseRounded />, path: "/" },
+    { icon: <FactoryRounded />, path: "/npoa/1" },
+    { icon: <LayersRounded />, path: "/npoa/2" },
+    { icon: <TodayRounded />, path: "/npoa/3" },
+    { icon: <DescriptionRounded />, path: "/npoa/4" },
+    { icon: <ShowChartRounded />, path: "/npoa/5" },
+  ];
+  console.log(pathName);
+
+  return (
+    <nav className={styles.navBar}>
+      {PAGES.map(({ icon, path }) => (
+        <Link
+          key={path}
+          href={path}
+          className={`${styles.navItem} ${
+            pathName === path ? styles.activeNavItem : ""
+          }`}
+        >
+          {icon}
+        </Link>
+      ))}
+    </nav>
+  );
+}
