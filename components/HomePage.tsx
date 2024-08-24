cf90debdc75670c64d5fecdf9ea849363ad6af94
suneelfm <sfm.20031998@gmail.com>
2024-08-24 22:34:40 +0530

    Created Machine details Card

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -4,11 +4,15 @@ import {
   ArrowDropDownOutlined,
   ArrowDropUpOutlined,
   HelpOutlineRounded,
+  Search,
 } from "@mui/icons-material";
-import { Box, Card, Grid, Typography } from "@mui/material";
+import { Box, Card, Grid, LinearProgress, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import styles from "../styles/page.module.css";
 import { Chart, ChartItem } from "chart.js/auto";
+import MachineDetailChip from "./MachineDetailChip";
+import MachineTimingCard from "./MachineTimingCard";
+import TimeTick from "./TimeTick";
 
 export default function HomePage() {
   useEffect(() => {
@@ -141,40 +145,16 @@ export default function HomePage() {
                 ></canvas>
               </Grid>
               <Grid item xs={6} p={2}>
-                <Card
-                  className={styles.productivityTimeCard}
-                  sx={{ borderLeft: "6px solid #4a7955" }}
-                >
-                  <Grid className={styles.productivityStatus}>Active</Grid>
-                  <Grid color={"white"}>2h 4m</Grid>
-                </Card>
+                <MachineTimingCard status="Active" time="2h 4m" />
               </Grid>
               <Grid item xs={6} p={2}>
-                <Card
-                  className={styles.productivityTimeCard}
-                  sx={{ borderLeft: "6px solid #44935c" }}
-                >
-                  <Grid className={styles.productivityStatus}>Loading</Grid>
-                  <Grid color={"white"}>2h 4m</Grid>
-                </Card>
+                <MachineTimingCard status="Loading" time="0m" />
               </Grid>
               <Grid item xs={6} p={2}>
-                <Card
-                  className={styles.productivityTimeCard}
-                  sx={{ borderLeft: "6px solid #cfc54c" }}
-                >
-                  <Grid className={styles.productivityStatus}>Setup</Grid>
-                  <Grid color={"white"}>2h 4m</Grid>
-                </Card>
+                <MachineTimingCard status="Setup" time="0m" />
               </Grid>
               <Grid item xs={6} p={2}>
-                <Card
-                  className={styles.productivityTimeCard}
-                  sx={{ borderLeft: "6px solid #964244" }}
-                >
-                  <Grid className={styles.productivityStatus}>Stalled</Grid>
-                  <Grid color={"white"}>2h 4m</Grid>
-                </Card>
+                <MachineTimingCard status="Stalled" time="8h 22m" />
               </Grid>
             </Grid>
           </Card>
@@ -221,6 +201,153 @@ export default function HomePage() {
                   </Grid>
                 </Card>
               ))}
+            </Grid>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card className={styles.card} sx={{ maxHeight: "300px !important" }}>
+            <Grid container className={styles.machineDetailsHead}>
+              <Grid item xs={1} paddingX={"5px"}>
+                Machines
+              </Grid>
+              <Grid item xs={1} paddingX={"5px"}>
+                Status
+              </Grid>
+              <Grid item xs={1} md={2} paddingX={"5px"}>
+                Cycle Progress
+              </Grid>
+              <Grid item xs={2} paddingX={"5px"}>
+                Production Progress
+              </Grid>
+              <Grid item xs={6} md={5} paddingX={"5px"}>
+                Production Remaining
+              </Grid>
+            </Grid>
+            <Grid container color={"white"} alignItems={"center"} pt={1}>
+              <Grid item xs={1} paddingX={"5px"} fontSize={"12px"}>
+                HALO - 01
+              </Grid>
+              <Grid item xs={1} paddingX={"5px"}>
+                <div
+                  className={styles.statusChip}
+                  style={{ color: status === "active" ? "green" : "white" }}
+                >
+                  IDLE
+                </div>
+              </Grid>
+              <Grid item xs={1} md={2} paddingX={"5px"}>
+                <LinearProgress
+                  color="success"
+                  className={styles.progressBar}
+                  variant="determinate"
+                  value={0}
+                />
+              </Grid>
+              <Grid
+                item
+                container
+                xs={2}
+                fontSize={"12px"}
+                paddingX={"5px"}
+                alignItems={"center"}
+              >
+                <LinearProgress
+                  color="success"
+                  className={styles.progressBar}
+                  variant="determinate"
+                  value={100}
+                />
+                100%
+              </Grid>
+              <Grid item xs={6} md={5} paddingX={"5px"}></Grid>
+            </Grid>
+            <Grid container py={2}>
+              <Grid container item xs={5}>
+                <Grid item container xs={6} px={1}>
+                  <Grid item xs={4} padding={"2px 4px"}>
+                    <MachineDetailChip detail="MOM" detailTitle={"MODE"} />
+                  </Grid>
+                  <Grid item xs={4} padding={"2px 4px"}>
+                    <MachineDetailChip detail="09001" detailTitle="PROGRAM" />
+                  </Grid>
+                  <Grid item xs={4} padding={"2px 4px"}>
+                    <MachineDetailChip detailTitle="TOOL" detail={1} />
+                  </Grid>
+                  <Grid item xs={4} padding={"2px 4px"}>
+                    <MachineDetailChip detailTitle="PARTS" detail={188} />
+                  </Grid>
+                  <Grid item xs={4} padding={"2px 4px"}>
+                    <MachineDetailChip detailTitle="PART GOAL" detail={0} />
+                  </Grid>
+                  <Grid item xs={4} padding={"2px 4px"}>
+                    <MachineDetailChip detailTitle="SPINDLE" detail={0} />
+                  </Grid>
+                </Grid>
+                <Grid item container xs={6} px={1}>
+                  <Grid item xs={6} padding={"5px 6px"}>
+                    <MachineTimingCard status="Active" time="2h 4m" />
+                  </Grid>
+                  <Grid item xs={6} padding={"5px 6px"}>
+                    <MachineTimingCard status="Loading" time="0m" />
+                  </Grid>
+                  <Grid item xs={6} padding={"5px 6px"}>
+                    <MachineTimingCard status="Setup" time="0m" />
+                  </Grid>
+                  <Grid item xs={6} padding={"5px 6px"}>
+                    <MachineTimingCard status="Stalled" time="8h 22m" />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item display={"flex"} xs={7}>
+                <Grid
+                  width={"calc(100% - 25px)"}
+                  height={"85px"}
+                  overflow={"auto"}
+                  border={"1px solid #333333"}
+                >
+                  <Grid position={"relative"} width={"1440px"} height={"50px"}>
+                    <Grid
+                      position={"absolute"}
+                      height={"100%"}
+                      width={"100px"}
+                      left={"100px"}
+                      bgcolor={"red"}
+                    />
+                    {Array.from(Array(24)).map((e, i) => (
+                      <TimeTick
+                        key={`tick-${i}`}
+                        time={`${i - 12 <= 0 ? i + "AM" : i - 12 + "PM"}`}
+                        position={i * 60}
+                      />
+                    ))}
+                    <TimeTick position={200} time="Now" />
+                  </Grid>
+                  <Grid
+                    width={"1440px"}
+                    height={"5px"}
+                    bgcolor={"#333333"}
+                  ></Grid>
+                </Grid>
+                <Grid
+                  height={"85px"}
+                  width={"25px"}
+                  border={"1px solid #333333"}
+                >
+                  <Grid
+                    textAlign={"right"}
+                    color={"#606060"}
+                    fontSize={"15px"}
+                    height={"50px"}
+                  >
+                    <Search fontSize="inherit" color="inherit" />
+                  </Grid>
+                  <Grid
+                    width={"100%"}
+                    height={"5px"}
+                    bgcolor={"#333333"}
+                  ></Grid>
+                </Grid>
+              </Grid>
             </Grid>
           </Card>
         </Grid>
---
+++ components/TimeTick.tsx
@@ -0,0 +1,37 @@
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+export default function TimeTick({
+  time,
+  position,
+}: {
+  time: string;
+  position: number;
+}) {
+  const [left, setLeft] = useState(0);
+  useEffect(() => {
+    const width = document.getElementById("tickLabel")?.clientWidth ?? 0;
+    setLeft(width / 2);
+  }, []);
+
+  return (
+    <Grid
+      id={time}
+      position={"absolute"}
+      borderLeft={time === "Now" ? "2px solid #bca179" : "1px solid #33333380"}
+      left={`${position}px`}
+      height={"120%"}
+    >
+      <Grid
+        id="tickLabel"
+        position={"absolute"}
+        top={"105%"}
+        left={`-${left}px`}
+        fontSize={"10px"}
+        color={"#606060"}
+      >
+        {time}
+      </Grid>
+    </Grid>
+  );
+}
