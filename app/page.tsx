93361a2554e5527d0c19197b9b877de4991fe9d9
suneelfm <sfm.20031998@gmail.com>
2024-08-24 14:09:24 +0530

    Added small cards with static content

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Box, Card, Grid, Typography } from "@mui/material";
+import styles from "./page.module.css";
+import {
+  ArrowDropDownOutlined,
+  ArrowDropUpOutlined,
+  HelpOutlineRounded,
+} from "@mui/icons-material";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <Box>
+      <Grid container spacing={2}>
+        {[1, 2, 3, 4, 5, 6].map((i) => (
+          <Grid key={i} item xs={12} md={4} lg={2}>
+            <Card className={styles.card}>
+              <Grid container justifyContent={"flex-end"}>
+                <HelpOutlineRounded className={styles.helpIcon} />
+              </Grid>
+              <Typography variant="h4" className={styles.content}>
+                20%
+              </Typography>
+              <Grid container alignItems={"center"}>
+                <span className={styles.label}>Active</span>
+                <span
+                  className={styles.improvement}
+                  style={{ color: true ? "#4a7955" : "#964244" }}
+                >
+                  {true ? (
+                    <ArrowDropUpOutlined sx={{ fontSize: "15px" }} />
+                  ) : (
+                    <ArrowDropDownOutlined sx={{ fontSize: "15px" }} />
+                  )}
+                  20%
+                </span>
+              </Grid>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
